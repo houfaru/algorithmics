270a5590419b7f576ba1ca989ee0b45b9b189e68
Fajar Haifani <fajar.haifani@e2open.com>
2019-08-25 02:55:40 +0200

    add NP optimization interface

---
+++ algorithmics/np-core-interfaces/com/algorithmics/npo/core/NPOProblem.java
@@ -0,0 +1,8 @@
+package com.algorithmics.npo.core;
+
+import com.algorithmics.np.core.Certificate;
+import com.algorithmics.np.core.NPProblem;
+
+public interface NPOProblem<C extends Certificate> extends NPProblem<C> {
+    public double opt();
+}
